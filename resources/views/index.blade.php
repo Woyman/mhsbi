0106feb4e2c8c2a3fbe4864c39698821aacbd5c3
Woyman <woyman11@gmail.com>
2022-01-28 00:52:00 +0700

    about me section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,83 @@
     </div>
 </section><!-- End Hero -->
 
+<main id="main">
+    
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>About Me</h2>                
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="{{ asset('assets/img/my-picture.jpg') }}" class="img-fluid" alt="">
+                </div>
+                <div class="col-lg-8 pt-4 pt-lg-0 content">
+                    <h3> Web Developer.</h3>
+                    <p>
+                        Lives in Yogyakarta, Indonesia. 
+                        Graduated from the Department of Informatics Engineering at Amikom University Yogyakarta. 
+                        Now I work as a Web Developer at Inosoft Transistem.
+                    <br>
+                        I started into web development since Vocational High School. 
+                        Skilled in using PHP, Laravel, Mongodb. And then worked as a full stack developer for Inosoft, focusing more on back-end development.
+                    </p>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <h4>Languages</h4>
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> PHP </li>                                                                                  
+                                <li><i class="bi bi-chevron-right"></i> Javascript  </li>                            
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                            <h4>Database</h4>
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> Mysql </li>                            
+                                <li><i class="bi bi-chevron-right"></i> MongoDb  </li>                            
+                                
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                            <h4>Frontend</h4>
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> Bootstrap </li>                            
+                                <li><i class="bi bi-chevron-right"></i> CSS & HTML  </li>      
+                                <li><i class="bi bi-chevron-right"></i> Vuejs  </li>      
+                                
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                            <h4>Backend</h4>
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> Rest API </li>                            
+                                <li><i class="bi bi-chevron-right"></i> Laravel  </li>      
+                                <li><i class="bi bi-chevron-right"></i> Nginx  </li>      
+                                
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                            <h4>Tools</h4>
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> VSCode </li>                            
+                                <li><i class="bi bi-chevron-right"></i> Laragon  </li>      
+                                <li><i class="bi bi-chevron-right"></i> Postman  </li>      
+                                <li><i class="bi bi-chevron-right"></i> Git  </li>      
+                                
+                            </ul>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</main>
+
 
 
 @endsection
