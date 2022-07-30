18c757413725c009fa5edabf418a57ba70b9b435
woyman <woyman11@gmail.com>
2022-07-31 01:17:50 +0700

    update meta wedding

--- resources/views/layout/wedding/meta-header.blade.php
+++ resources/views/layout/wedding/meta-header.blade.php
@@ -6,14 +6,18 @@
 <meta content="" name="keywords">
 
 <meta property="og:type" content="website">
+<meta property="og:site_name" content="mhsbi.com">
 <meta property="og:url" content="https://mhsbi.com/my-wedding-invitation">
-<meta property="og:title" content="Aini & Hasbi Wedding Invitation">
-<meta property="og:description" content="">
+<meta property="og:title" content="Aini dan Hasbi Wedding Invitation">
+<meta property="og:description" content="Undangan pernikahan Aini dan Hasbi">
 <meta property="og:image" itemprop="image" content="<?php echo asset('assets/img/tumbnail-meta.jpg') ?>">
+<meta property="og:image:width" content="1903" />
+<meta property="og:image:height" content="1010" />
+<meta property="og:image:type" content="image/jpeg" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="https://mhsbi.com/my-wedding-invitation">
-<meta property="twitter:title" content="Aini & Hasbi Wedding Invitation">
-<meta property="twitter:description" content="">
+<meta property="twitter:title" content="Aini dan Hasbi Wedding Invitation">
+<meta property="twitter:description" content="Undangan pernikahan Aini dan Hasbi">
 <meta property="twitter:image" content="<?php echo asset('assets/img/tumbnail-meta.jpg') ?>">
