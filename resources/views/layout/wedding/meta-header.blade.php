46da9b3afdb4b65c5711b15dc8eab7e9f49d6954
woyman <woyman11@gmail.com>
2022-08-01 19:17:24 +0700

    update tumbnail meta og

--- resources/views/layout/wedding/meta-header.blade.php
+++ resources/views/layout/wedding/meta-header.blade.php
@@ -11,9 +11,9 @@
 <meta property="og:url" content="https://mhsbi.com/my-wedding-invitation">
 <meta property="og:title" content="Aini dan Hasbi Wedding Invitation">
 <meta property="og:description" content="Undangan pernikahan Aini dan Hasbi">
-<meta property="og:image" itemprop="image" content="<?php echo asset('assets/img/tumbnail-meta.jpg') ?>">
-<meta property="og:image:width" content="1000" />
-<meta property="og:image:height" content="530" />
+<meta property="og:image" itemprop="image" content="<?php echo asset('assets/img/my-wedding-tumbnail.jpg') ?>">
+<meta property="og:image:width" content="360" />
+<meta property="og:image:height" content="408" />
 <meta property="og:image:type" content="image/jpeg" />
 
 <!-- Twitter -->
@@ -21,6 +21,6 @@
 <meta property="twitter:url" content="https://mhsbi.com/my-wedding-invitation">
 <meta property="twitter:title" content="Aini dan Hasbi Wedding Invitation">
 <meta property="twitter:description" content="Undangan pernikahan Aini dan Hasbi">
-<meta property="twitter:image" content="<?php echo asset('assets/img/tumbnail-meta.jpg') ?>">
+<meta property="twitter:image" content="<?php echo asset('assets/img/my-wedding-tumbnail.jpg') ?>">
 
 <meta name="google-site-verification" content="OIH3RWP4XQ9wPMGIc5FCdDY_6ZY0NBdb5hNDH2h1-s4" />
